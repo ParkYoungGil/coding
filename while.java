ac885ef14d81f8ba49e65ae6f7c399c36a5f5b4a
ParkYoungGil <eclipse9128@naver.com>
2023-04-15 20:12:52 +0900

    Score Modified 0.1

--- while.java
+++ while.java
@@ -3,14 +3,26 @@ import java.util.Scanner;
 class While {
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("점수를  입려하시오.);
+		System.out.println("Input Score ! ");
 		int num = scanner.nextInt();
 
 		if (num == 100){
-			System.out.println("만점 A+ 학점입니다.");
+			System.out.println("Your Score : " + num + "    A+ Very Very Good !!");
 		}
-		else if ((num < 100) || (num < 90)){
-			System.out.println(num + "점 이시군요 A 학점입니다.");
+		else if ((100 > num) && (num > 90)){
+			System.out.println("Your Score : " + num + "    A  VeryGood !!");
+		}
+		else if ((num < 90) && (num > 80)){
+			System.out.println("Your Score : " + num + "    B+ So Good !!");
+		}
+		else if ((num < 80) && (num > 70)){
+			System.out.println("Your Score : " + num + "    B  Not Bad !!");
+		}
+		else if ((num > 100) || (num < 0)){
+			System.out.println("Re Input Please");
+		}
+		else {
+			System.out.println("Your Score : " + num + "    You are Stupid !!");
 		}
 	}
 }
