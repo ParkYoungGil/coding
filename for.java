a1c37e898bffd8445d5d8248543327a1d75fb1fd
ParkYoungGil <eclipse9128@naver.com>
2023-04-15 17:53:44 +0900

    JAVA_FOR_MODIFIED 0.2

--- for.java
+++ for.java
@@ -1,10 +1,10 @@
 class For{
 	public static void main(String[] args){
 		for(int i = 1; i < 7; i++){
-			System.out.println("*");
-			for(int j = 0 ; j < i; j++){
+			for(int j = 1 ; j < i; j++){
 				System.out.print("*");
 			}
+				System.out.println("*");
 		}
 	}
 }
